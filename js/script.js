67167e1a990fe3d2a14a35a1059c4da8be7f53af
vorning <victorvorningp@gmail.com>
2024-04-18 15:25:26 +0200

    Forside
    Forside, js, css, billeder sat ind.

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function toggleMenu() {
+    console.log("Menuen blev klikket"); // Tilføj denne linje for at kontrollere, om funktionen kaldes
+    var navbar = document.getElementById("navbar");
+    navbar.classList.toggle("active");
+}
