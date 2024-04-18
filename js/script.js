047bf31e80e0f90f9dbbe6ebf6f1127fd20383fd
vorning <82236004+vorning@users.noreply.github.com>
2024-04-18 17:50:12 +0200

    Forside fertig ish

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 function toggleMenu() {
-    console.log("Menuen blev klikket"); // Tilføj denne linje for at kontrollere, om funktionen kaldes
     var navbar = document.getElementById("navbar");
     navbar.classList.toggle("active");
 }
